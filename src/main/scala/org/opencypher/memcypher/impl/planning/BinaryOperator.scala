e8ddd831f8d9de013a723d4f0499cb05e4122c87
Florentin Dörre <florentin@owitsch.de>
2018-05-28 13:40:09 +0200

    fresh-start: id-function and filtering of groupby properties
    for each entity tupel (entity,groupbyList) ... constructedNodeExtended class isnt working for now

--- src/main/scala/org/opencypher/memcypher/impl/planning/BinaryOperator.scala
+++ src/main/scala/org/opencypher/memcypher/impl/planning/BinaryOperator.scala
@@ -21,11 +21,12 @@ import org.opencypher.memcypher.impl.value.CypherMapOps._
 import org.opencypher.memcypher.impl.{MemPhysicalResult, MemRuntimeContext}
 import org.opencypher.okapi.api.schema.Schema
 import org.opencypher.okapi.api.types.CTInteger
-import org.opencypher.okapi.api.value.CypherValue.{CypherBoolean, CypherInteger, CypherMap}
+import org.opencypher.okapi.api.value.CypherValue.{CypherBoolean, CypherInteger, CypherList, CypherMap, CypherString, CypherValue}
 import org.opencypher.okapi.impl.exception.NotImplementedException
+import org.opencypher.okapi.ir.api.Label
 import org.opencypher.okapi.ir.api.expr._
 import org.opencypher.okapi.ir.api.set.SetPropertyItem
-import org.opencypher.okapi.logical.impl.{ConstructedEntity, ConstructedNode, LogicalPatternGraph}
+import org.opencypher.okapi.logical.impl.{ConstructedEntity, ConstructedNode, ConstructedRelationship, LogicalPatternGraph}
 import org.opencypher.okapi.relational.impl.physical.{InnerJoin, JoinType, LeftOuterJoin, RightOuterJoin}
 import org.opencypher.okapi.relational.impl.table.RecordHeader
 
@@ -87,8 +88,17 @@ final case class TabularUnionAll(left: MemOperator, right: MemOperator) extends 
   }
 }
 
+class ConstructNodeExtended(v: Var, labels: Set[Label], baseEntity: Option[Var],
+                            groupBy: Set[Var],
+                            aggregatedProperties: Option[Set[SetPropertyItem[Expr]]]) extends ConstructedNode(v, labels, baseEntity){} //cant be used?? (cant be found)
+
+/*case class ConstructedRelationshipExtended(baseRelationship:ConstructedRelationship,
+                                           groupBy:Set[Var],
+                                           aggregatedProperties:Option[Set[SetPropertyItem[Expr]]])*/
+
 final case class ConstructGraph(left: MemOperator, right: MemOperator, construct: LogicalPatternGraph) extends BinaryOperator {
   val current_max_id = new AtomicLong()
+  var storedIDs = Map[String, Long]()
 
   //toString method from openCypher
   override def toString: String = {
@@ -104,110 +114,41 @@ final case class ConstructGraph(left: MemOperator, right: MemOperator, construct
     val matchTable = left.records
     val LogicalPatternGraph(schema, clonedVarsToInputVars, newEntities, sets, _, name) = construct
 
-    val clonedEntitiesTable = clonedVarsToInputVars.foldLeft(Seq.empty[CypherMap]) {
-      case (clonedEntities, (newVar, oldVar)) => clonedEntities ++ copyContents(oldVar, Option(newVar), matchTable).data.data.map(_.updated(newVar.name, CypherInteger(generateID())))
-    }
 
-    val entityTable = createEntities(newEntities, schema, matchTable)
-    val entityTableWithProperties = sets.foldLeft(entityTable) {
-      case (table, SetPropertyItem(key, v, expr)) => constructProperty(v, key, expr, table)(table.header, context)
-    }
-
-    val nodesUnited = entityTableWithProperties.data.unionAll(Embeddings(clonedEntitiesTable))
-
-    //Todo: MemRecords to MemGraph (use nestNode --> getHeaderRight!)
-    //---------------------------------------------------------------------
-    //straight-forward version (newEntities+their labels & properties)(without use of MemRecords)
-    val nodes_2 = newEntities.foldLeft(Seq.empty[MemNode]) {
-      (list, b) =>
-        b match {
-          case x: ConstructedNode =>
-            val labels = x.labels.map(_.name.toString)
-            val properties = sets.filter(_.variable == x.v).foldLeft(CypherMap.empty) {
-              (map, setItem) => map.updated(setItem.propertyKey, RichCypherMap(CypherMap.empty).evaluate(setItem.setValue)(RecordHeader.empty, context))
-            }
-            list :+ MemNode(generateID(), labels, properties)
-          case _ => list //println("not implemented for" + z.getClass)
+    val extendedEntities = newEntities.map(entity => {
+      val potentialGrouping = sets.filter(x => (x.variable == entity.v) && (x.propertyKey == "groupby"))
+      if (potentialGrouping.nonEmpty) {
+        val potentialGroupinglist = RichCypherMap(CypherMap.empty).evaluate(potentialGrouping.head.setValue)(RecordHeader.empty, MemRuntimeContext.empty)
+        potentialGroupinglist match {
+          case groupByList: CypherList => entity -> groupByList.value.map(_.toString()) //create tupel (ConstructedEntity,List[String])
+          case x => throw NotImplementedException("wrong value typ for groupBy: should be CypherList but found " + x.getClass)
         }
-    }
+      } else entity -> List[String]()
+    })
+
+    //todo: find expr for matchTable.data.project()
 
 
-    val newGraph = MemCypherGraph.create(nodes_2, Seq[MemRelationship]())
-    MemPhysicalResult(MemRecords.unit(), newGraph, name)
+    MemPhysicalResult(MemRecords.unit(), MemCypherGraph.empty, name)
   }
 
-  def createEntities(newEntities: Set[ConstructedEntity], schema: Schema, matchTable: MemRecords): MemRecords = {
 
-    val nodes = newEntities.collect {
-      case c@ConstructedNode(_, _, _) => c
-    }
-    var header = RecordHeader.empty
-    val nodeData = nodes.foldLeft(Seq[CypherMap]()) {
-      (generatedNodes, node) => {
-        header ++= RecordHeader.nodeFromSchema(node.v, schema) //useless now?
-        //todo baseNodes.header --> use to union with existing header!
-        val baseNodes = node.baseEntity match {
-          case Some(baseNode) => copyContents(baseNode, Option(node.v), matchTable)
-          case None => MemRecords(Embeddings.empty, RecordHeader.empty)
-        }
-        val newNode = CypherMap(node.labels.map(label => node.v.name + ":" + label.name -> CypherBoolean(true)).toMap)
-        if (baseNodes.data.data.nonEmpty) {
-          var newNodesWithbaseNodes = Seq[CypherMap]()
-          baseNodes.data.data.foreach(copiedNode => {
-            newNode.updated(node.v.name, generateID())
-            newNodesWithbaseNodes :+= (copiedNode ++ newNode)
-          }
-          )
-          generatedNodes ++ newNodesWithbaseNodes
-        }
-        else generatedNodes :+ newNode.updated(node.v.name, generateID())
-        //problem: both freshvars in Recordheader --> both vars must have an entry in one Cyphermap? (with header?)
+  def generateID(constructedEntityName: String, groupby: Seq[(String, Long)] = Seq()): Long = {
+    var key = constructedEntityName
+
+    if (groupby.nonEmpty) {
+      val sorted_groupby = groupby.sortWith((x, y) => x._1 < y._1); // groupby a,b should be equal to groupby b,a
+      key += sorted_groupby.foldLeft("")((x, tupel) => x + "|" + tupel._1 + tupel._2); //generate with groupby
+      if (storedIDs.contains(key)) storedIDs.getOrElse(key, -1)
+      else {
+        storedIDs += key -> current_max_id.incrementAndGet()
+        current_max_id.get()
       }
     }
-
-    MemRecords.create(Embeddings(nodeData), header)
-  }
-
-  // with grouping make with map (key = variablename|groupedbyvars(List(var,value))
-  def generateID(): Long = current_max_id.incrementAndGet()
-
-  //return memRecord --> header too
-  //maybe copy id as @oldID for later
-  def copyContents(baseEntity: Var, newEntity: Option[Var], baseRecords: MemRecords): MemRecords = {
-    implicit val header: RecordHeader = baseRecords.header
-    implicit val context: MemRuntimeContext = MemRuntimeContext.empty
-
-    val origLabelSlots = header.labelSlots(baseEntity).values.toSet
-    val origPropertySlots = header.propertySlots(baseEntity).values.toSet
-    val copySlotContents = origLabelSlots.map(_.withOwner(baseEntity)).map(_.content)
-    val explRow = baseRecords.data.data.head
-    val explKey = copySlotContents.head.key
-    val storedCypherValue = RichCypherMap(explRow).evaluate(explKey) //getValue of the row --> possible to get values for all keys nested in expr
-
-
-    val labelFields = baseRecords.data.columns.filter(_.matches(baseEntity.name + ":.*")) //use naming convention x.propertykey & x:label (todo: find better way)
-    val propertyFields = baseRecords.data.columns.filter(_.matches(baseEntity.name + "[.].*"))
-    val fieldsToCopy = labelFields ++ propertyFields
-    val baseData = baseRecords.data.select(fieldsToCopy)
-      .distinct(fieldsToCopy) //right Context?
-
-    val newData = newEntity match {
-      case Some(newEntity) => {
-        val renaming = fieldsToCopy.map(oldName => oldName -> oldName.replaceFirst(baseEntity.name, newEntity.name)).toMap
-        baseData.withColumnsRenamed(renaming)
-      }
-      case None => baseData
+    else {
+      val newID = current_max_id.incrementAndGet()
+      storedIDs += key + newID -> newID //everytime id fct for ungrouped var gets called --> new ID
+      newID
     }
-
-    MemRecords(newData, header)
-  }
-
-  def constructProperty(variable: Var, propertyKey: String, propertyValue: Expr, constructedTable: MemRecords)(implicit header: RecordHeader, context: MemRuntimeContext): MemRecords = {
-    val dataToUpdate = constructedTable.data.filter(IsNotNull(variable)(CTInteger))
-    val dataWithProperty = dataToUpdate.data.map(_.updated(variable.name + "." + propertyKey, RichCypherMap(CypherMap.empty).evaluate(propertyValue))) //use Columnname funktion? (would get escaped names)
-
-    //fixes problem that entities with no new property are left out (find better way!)
-    val dataWithNoProperty = constructedTable.data.data.diff(dataToUpdate.data)
-    MemRecords.create(dataWithProperty ++ dataWithNoProperty, constructedTable.header)
   }
 }
