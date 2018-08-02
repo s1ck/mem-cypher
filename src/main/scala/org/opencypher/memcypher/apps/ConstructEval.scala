0c8da97c04a7a7a997468011dee0749d075e4b70
Florentin Dörre <florentin@owitsch.de>
2018-08-02 23:18:05 +0200

    better time measurments; solved memory issue with MemSession

--- src/main/scala/org/opencypher/memcypher/apps/ConstructEval.scala
+++ src/main/scala/org/opencypher/memcypher/apps/ConstructEval.scala
@@ -4,12 +4,14 @@ import com.typesafe.scalalogging.Logger
 import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator
 import org.opencypher.memcypher.api.value.MemNode
 import org.opencypher.memcypher.api.{MemCypherGraph, MemCypherSession}
+import org.opencypher.memcypher.apps.ConstructEval.logger
 import org.opencypher.okapi.api.configuration.Configuration.PrintTimings
 import org.opencypher.okapi.api.value.CypherValue.CypherMap
 
 object ConstructEval extends App {
   //function to get a specific number of nodes (size of the dataset)
   def getData(nodeNumber: Int) = {
+    implicit val memCypher: MemCypherSession = MemCypherSession.create
     var nodes: Seq[MemNode] = Seq.empty
     val r = scala.util.Random //random number oder über x mod n deterministisch die values bestimmen?
     for (x <- 1 to nodeNumber) {
@@ -20,19 +22,19 @@ object ConstructEval extends App {
   }
 
   def regardingInputCardinality(stepsize: Int, steps: Int) = {
+    implicit val memCypher: MemCypherSession = MemCypherSession.create
     val query =
       s"""|MATCH (b)
           |Construct New ({groupby:1})
           |RETURN Graph""".stripMargin
     var nodes: Seq[MemNode] = Seq.empty
-    for (x <- 0 to steps) {
+    for (x <- 1 to steps) {
       var dataSize = x * stepsize
-      if(x==0) dataSize = 1
       logger.info(s"Executing query on $dataSize matches")
       //redundant code but reduces unnecessary node creations?
       val r = scala.util.Random //random number oder über x mod n deterministisch die values bestimmen?
       for (y <- 1 to stepsize) {
-        nodes :+= MemNode(y+dataSize, Set.empty, CypherMap("two" -> r.nextInt(2), "five" -> r.nextInt(5), "ten" -> r.nextInt(10),
+        nodes :+= MemNode(y + dataSize, Set.empty, CypherMap("two" -> r.nextInt(2), "five" -> r.nextInt(5), "ten" -> r.nextInt(10),
           "twenty" -> r.nextInt(20), "fifty" -> r.nextInt(50), "hundred" -> r.nextInt(100)))
       }
       val graph = MemCypherGraph.create(nodes, Seq.empty)
@@ -41,10 +43,10 @@ object ConstructEval extends App {
     }
   }
 
-  def regardingOutputCardinality(outputCardinality: List[Any],graph: MemCypherGraph) = {
+  def regardingOutputCardinality(outputCardinality: List[Any], graph: MemCypherGraph) = {
     val matchPattern = "MATCH (b) "
     val returnPattern = "Return Graph"
-
+    val graphsize = graph.nodes.length
     outputCardinality.foreach(
       grouping => {
         val string = grouping match {
@@ -54,9 +56,10 @@ object ConstructEval extends App {
         }
         val constructPattern = s"Construct New ({groupby:$string})"
         val query = matchPattern + constructPattern + returnPattern
-        logger.info(query + s" on ${graph.nodes.length} matches")
-        val result = graph.cypher(query).getRecords
-        logger.info("size " + ObjectSizeCalculator.getObjectSize(result.collect) + " bytes")
+        logger.info(query + s" on $graphsize matches")
+        val result = graph.cypher(query)
+        logger.info("record size " + ObjectSizeCalculator.getObjectSize(result.getRecords.collect) + " bytes")
+        logger.info("graph size with " + result.getGraph.nodes("n").size + " nodes " + ObjectSizeCalculator.getObjectSize(result.getGraph) + " bytes")
       }
     )
   }
@@ -65,40 +68,50 @@ object ConstructEval extends App {
     val matchPattern = "MATCH (b) "
     val returnPattern = "Return Graph"
     var properties = ""
+    val graphsize = graph.nodes.length
     for (x <- 1 to maxNumber) {
       properties += s"""aggr$x :"$aggregator","""
       val constructPattern = s"Construct New ({groupby:1,${properties.substring(0, properties.length - 1)}})"
       val query = matchPattern + constructPattern + returnPattern
-      logger.info(s"$x aggregated properties on ${graph.nodes.length} matches")
-      graph.cypher(query)
+      logger.info(s"$x aggregated properties on $graphsize matches")
+      val result = graph.cypher(query)
+      //logger.info("record size " + ObjectSizeCalculator.getObjectSize(result.getRecords.collect) + " bytes")
+      //logger.info("graph size with "+result.getGraph.nodes("n").size+" nodes " + ObjectSizeCalculator.getObjectSize(result.getGraph) + " bytes")
     }
   }
 
-  def regardingNodeNumber(maxNumber: Int, graph: MemCypherGraph) = {
+  def regardingNodeNumber(maxNumber: Int, graph: MemCypherGraph,aggregated:Boolean) = {
     val matchPattern = "MATCH (x) "
     val returnPattern = "Return Graph"
     var nodes = ""
+    val node:String = {if(aggregated) s"""({groupby:"x.ten",what:"avg(x)"}),""" else s"""(),"""}
+    val graphsize = graph.nodes.length
     for (x: Int <- 1 to maxNumber) {
-      nodes += s"""(),"""
+      nodes += node
       val constructPattern = s"Construct New ${nodes.substring(0, nodes.length - 1)}"
       val query = matchPattern + constructPattern + returnPattern
-      logger.info(s"$x nodes on ${graph.nodes.length} matches")
-      val result = graph.cypher(query).getRecords
-      logger.info("size " + ObjectSizeCalculator.getObjectSize(result.collect) + " bytes") //linearer Zusammenhang, bis auf sprung von 1980 zu 52688 ?
+      logger.info(s"$x nodes on $graphsize matches")
+      val result = graph.cypher(query)
+      //logger.info("record size " + ObjectSizeCalculator.getObjectSize(result.getRecords.collect) + " bytes")
+      //logger.info("graph size with " + result.getGraph.nodes("n").size + " nodes " + ObjectSizeCalculator.getObjectSize(result.getGraph) + " bytes")
 
     }
   }
 
-  def regardingEdgeNumber(maxNumber: Int, graph: MemCypherGraph) = {
+  def regardingEdgeNumber(maxNumber: Int, graph: MemCypherGraph,aggregated:Boolean) = {
     val matchPattern = "MATCH (x) "
     val returnPattern = "Return Graph"
     var edges = ""
+    val edge = {if(aggregated) s"""(a)-[:filler]->(b),""" else s"""(a)-[:filler{groupby:"b.ten",what:"avg(x)"]]->(b),"""}
+    val graphsize = graph.nodes.length
     for (x <- 1 to maxNumber) {
-      edges += s"""(a)-[:filler]->(b),"""
+      edges += edge
       val constructPattern = s"Construct New (a),(b),${edges.substring(0, edges.length - 1)}"
       val query = matchPattern + constructPattern + returnPattern
-      logger.info(s"$x edges and 2 nodes on ${graph.nodes.length} matches")
-      graph.cypher(query)
+      logger.info(s"$x edges and 2 nodes on $graphsize matches")
+      val result = graph.cypher(query)
+      //logger.info("record size " + ObjectSizeCalculator.getObjectSize(result.getRecords.collect) + " bytes")
+      //logger.info("graph size " + ObjectSizeCalculator.getObjectSize(result.getGraph) + " bytes")
     }
   }
 
@@ -113,25 +126,33 @@ object ConstructEval extends App {
     logger.info("simple: " + ObjectSizeCalculator.getObjectSize(result_simple) + " pattern: " + ObjectSizeCalculator.getObjectSize(result_motive))
   }
 
+  //todo: when testing graph based approach ... also look at memory of graph (alter tests therefore)
+  //todo: when testing size of graph ... without loop ? (memcypherSession wird größer)
   val logger = Logger("Construct Evaluation")
   //PrintTimings.set()
-  implicit val memCypher: MemCypherSession = MemCypherSession.create
-  val warmupGraph = getData(1000)
-  val realGraph = getData(100000)
-  regardingInputCardinality(100,20) //warmup
-  regardingInputCardinality(50000, 10)
-  regardingOutputCardinality(List(1,"b.two","b.five","b.ten","b.twenty","b.fifty","b.hundred","b"),warmupGraph) //warmup
-  regardingOutputCardinality(List(1,"b.two","b.five","b.ten","b.twenty","b.fifty","b.hundred","b"),realGraph)
-  regardingOutputCardinality(List(List("b.two","b.five"),List("b.two","b.ten"),List("b.two","b","b.five","b.ten")),warmupGraph) //warmup
-  regardingOutputCardinality(List(List("b.two","b.five"),List("b.two","b.ten"),List("b.two","b.five","b.ten"),List("b.two","b.hundred"),List("b.five","b.hundred"),List("b.ten","b.hundred"),List("b.twenty","b.hundred"),List("b.fifty","b.hundred"),List("b.ten","b.twenty","b.hundred"),List("b.ten","b.fifty","b.hundred"),List("b"),List(),List("b","b.ten"),List("b","b.ten","b.fifty","b.hundred")),realGraph) //todo more Lists
-  regardingAggregatedProperties("collect(b)",5,warmupGraph) //warmup
-  regardingAggregatedProperties("collect(b)",20,realGraph)
-  regardingNodeNumber(10,warmupGraph)
-  regardingNodeNumber(10,realGraph)
-  regardingEdgeNumber(10,warmupGraph)
-  regardingEdgeNumber(10,realGraph)
+
+
+  for (x <- 1 to 10) {
+    PrintTimings.set()
+    implicit val memCypher: MemCypherSession = MemCypherSession.create
+    val warmupGraph = getData(100)
+    val realGraph = getData(100000)
+    //regardingInputCardinality(100,20) //warmup
+    //regardingInputCardinality(50000, 10)
+    //regardingOutputCardinality(List(1, "b.two", "b.five", "b.ten", "b.twenty", "b.fifty", "b.hundred", "b"), warmupGraph) //warmup
+    //regardingOutputCardinality(List(List("b.two", "b.five"), List("b.two", "b.ten"), List("b.two", "b", "b.five", "b.ten")), warmupGraph) //warmup
+    //regardingOutputCardinality(List(1, "b.two", "b.five", "b.ten", "b.twenty", "b.fifty", "b.hundred"), realGraph)
+    //regardingOutputCardinality(List(List("b.two", "b.hundred"), List("b.five", "b.hundred"), List("b.ten", "b.hundred"), List("b.twenty", "b.hundred"), List("b.fifty", "b.hundred"), List("b.ten", "b.twenty", "b.hundred"), List("b.ten", "b.fifty", "b.hundred"), List("b"), List()), realGraph) //todo more Lists
+  //todo test copy
+  //regardingAggregatedProperties("avg(b)",10,warmupGraph) //warmup
+  //regardingAggregatedProperties("avg(b)",20,realGraph)
+  //regardingNodeNumber(10,warmupGraph,true)
+  //regardingNodeNumber(10,realGraph,true)//}
+  //regardingEdgeNumber(10,warmupGraph,true)
+  //regardingEdgeNumber(10,realGraph,true)
   motivetheory(warmupGraph)
   motivetheory(realGraph)
+}
 }
 
 
